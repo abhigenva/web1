c67801ccbda504d365a42747c922ce5cd882a1e1
Genva <inabhishek009@gmail.com>
2018-09-22 20:44:09 +0530

    Web page updated.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react';
-import bg from './bg.jpg';
 import './App.css';
-import logoprime from './logoprime.png';
-import download from './download.png';
-import sample from './sample.png';
 import sample2 from './sample2.png';
+import worldmap from './worldmap.jpg';
+import cover from './cover.png';
+
 
 class App extends Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
   render() {
     return (
       <div className="App">
       <div className="samp">
-        <img src={sample2} alt="sample2"/>
-        <img src={sample} alt="sample"/>
+        <h1>Norah</h1>
+        <img src={cover} alt="cover"/>
       </div>
-        <h1 className="Text">Welcome to Norah</h1>
-        <div className="logobrand">
-          <img src={logoprime} alt="logoprime"/>
-        </div>
-        <div className="BGImage">
-          <img src={bg} alt="bg"/>
-          <div className="store">
-            <img src={download} alt="download"/>
-          </div>
-        </div>
       </div>
     );
   }
