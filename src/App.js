e9e8c9ca0141267d3055ce674e4350dbed5a8633
Genva <inabhishek009@gmail.com>
2018-10-19 04:16:15 +0530

    Added paragraph.

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
               <a href="NAME"><h4>Contact</h4></a>
               </li>
               <li>
-              <h4>Blog</h4>
+              <a href="Blog"><h4>Blog</h4></a>
               </li>
             </ul>
             </div>
@@ -36,6 +36,8 @@ class App extends Component {
       <div classname="img">
         <img src={cover} alt="cover" border="5"/>
       </div>
+      <p>Pellentesque euismod, tortor in dictum fermentum, magna turpis laoreet massa, ac venenatis elit est ac metus. Ut gravida consequat metus ut maximus. Morbi egestas ac dui eu facilisis. Nulla iaculis, neque quis vestibulum volutpat, orci mi vulputate ex, in varius sem urna sed augue. Nullam lobortis commodo mauris, a ornare metus consectetur vitae. Pellentesque in nulla vel nunc mollis rhoncus. Donec dictum accumsan orci, a fermentum elit vulputate sed. Praesent convallis quis odio a auctor. In a felis leo. Praesent iaculis velit sit amet sem luctus lacinia. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+      <a href="#"><h3>Back to Top</h3></a>
       </div>
       </div>
     );
