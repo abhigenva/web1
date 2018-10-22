8b6886db45870db474f573a822ea499eeac758a7
Genva <inabhishek009@gmail.com>
2018-10-22 06:51:28 +0530

    update.

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
       <div classname="img">
         <img src={cover} alt="cover" border="5"/>
       </div>
-      <p>Pellentesque euismod, tortor in dictum fermentum, magna turpis laoreet massa, ac venenatis elit est ac metus. Ut gravida consequat metus ut maximus. Morbi egestas ac dui eu facilisis. Nulla iaculis, neque quis vestibulum volutpat, orci mi vulputate ex, in varius sem urna sed augue. Nullam lobortis commodo mauris, a ornare metus consectetur vitae. Pellentesque in nulla vel nunc mollis rhoncus. Donec dictum accumsan orci, a fermentum elit vulputate sed. Praesent convallis quis odio a auctor. In a felis leo. Praesent iaculis velit sit amet sem luctus lacinia. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+      
       <a href="#"><h3>Back to Top</h3></a>
       </div>
       </div>
